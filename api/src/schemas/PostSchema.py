808faf905cc8213188883fe8399a5f0c5ea04878
eokrizz <crisvagnersd@gmail.com>
2023-02-10 23:58:10 -0300

    reorganizando arquivos

--- api/src/schemas/PostSchema.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-from .UserSchema import UserSchema
-
-
-class PostSchema(Schema):
-    id = fields.Int(dump_only=True)
-    title = fields.Str(required=True)
-    content = fields.Str(required=True)
-    author = fields.Nested(UserSchema, only=('id', 'email'))
